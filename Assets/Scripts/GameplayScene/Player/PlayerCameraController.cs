1b3d64afe8d27363340e365d7cee37c449d33e5f
Sandra Ciudad <sandra.ciudad1@alu.uclm.es>
2024-12-24 11:20:03 +0100

    create player and define movements

---
+++ Assets/Scripts/GameplayScene/Player/PlayerCameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] Transform playerBody;  
+    
+    float mouseSensitivity = 100f;  
+    float verticalClamp = 80f;  
+    float xRotation = 0f;  
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;  
+        Cursor.visible = false; 
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -verticalClamp, verticalClamp);  
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);  
+    }
+}
---
+++ Assets/Scripts/GameplayScene/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+    Vector3 velocity;
+    bool isGrounded;
+
+    float speed = 5.0f;
+    float jumpHeight = 1.0f;
+    float gravity = -9.81f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (moveX != 0 || moveZ != 0)
+        {
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);  
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("jump", true);
+        } else
+        {
+            animator.SetBool("jump", false);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
