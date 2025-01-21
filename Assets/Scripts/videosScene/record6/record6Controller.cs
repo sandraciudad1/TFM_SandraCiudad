0bbc129f5dcde577c1b976995ff701cd77072824
Sandra Ciudad <sandra.ciudad1@alu.uclm.es>
2025-01-21 11:12:14 +0100

    add record6 animations

--- Assets/Scripts/videosScene/record6/record6Controller.cs
+++ Assets/Scripts/videosScene/record6/record6Controller.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class record6Controller : MonoBehaviour
 {
+    [SerializeField] CinemachineVirtualCamera vcam6;
+    [SerializeField] CinemachineVirtualCamera vcam7;
+
     AudioSource audio;
     Animator animator;
+    Animator vfxAnimator;
 
+    [SerializeField] GameObject vfx;
     [SerializeField] GameObject sampleFlag;
     [SerializeField] GameObject staticSample;
     bool activate = false;
 
-    // 
+    // Initializes components and starts coroutine.
     void Start()
     {
         audio = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
-        
+        vfxAnimator = vfx.GetComponent<Animator>();
+
+        setCamPriority(1, 0);
         StartCoroutine(waitToStart());
     }
 
-    // 
+    // Activates static sample if the flag is active.
     void Update()
     {
         if (sampleFlag.activeInHierarchy && !activate)
@@ -30,16 +38,25 @@ public class record6Controller : MonoBehaviour
         }
     }
 
-    // 
+    // Waits, triggers animations, changes cameras, and plays audio.
     IEnumerator waitToStart()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.5f);
         animator.SetBool("place", true);
         yield return new WaitForSeconds(6.13f);
         animator.SetBool("place", false);
-
-
-        //audio.Play();
+        yield return new WaitForSeconds(1.5f);
+        setCamPriority(0, 1);
+        yield return new WaitForSeconds(3.5f);
+        vfxAnimator.SetBool("scale", true);
+        yield return new WaitForSeconds(1f);
+        audio.Play();
     }
 
+    // Sets camera priority.
+    void setCamPriority(int cam1, int cam2)
+    {
+        vcam6.Priority = cam1;
+        vcam7.Priority = cam2;
+    }
 }
