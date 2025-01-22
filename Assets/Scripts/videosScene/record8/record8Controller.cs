8c43b70e93d1a732f1455241d7520659a0677fe8
Sandra Ciudad <sandra.ciudad1@alu.uclm.es>
2025-01-22 12:19:21 +0100

    add record8 animations

--- Assets/Scripts/videosScene/record8/record8Controller.cs
+++ Assets/Scripts/videosScene/record8/record8Controller.cs
@@ -8,28 +8,48 @@ public class record8Controller : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera vcam9;
     [SerializeField] CinemachineVirtualCamera vcam10;
 
+    Quaternion nicoleRotation = Quaternion.Euler(new Vector3(0f, -63.2f, 0f));
+    Quaternion nicoleRotation2 = Quaternion.Euler(new Vector3(0f, 92f, 0f));
     [SerializeField] GameObject nicole;
-    [SerializeField] GameObject cooper;
     Animator nicoleAnimator;
-    Animator cooperAnimator;
     AudioSource nicoleAudio;
+    bool rotateNicole = false;
+    bool rotateNicole2 = false;
+    bool pressBtn = false;
+
+    Vector3 targetPosition = new Vector3(118.848f, 20.66516f, 50.796f);
+    [SerializeField] GameObject cooper;
+    Animator cooperAnimator;
     AudioSource cooperAudio;
+    float moveSpeed = 0.5f;
+    float rotationSpeed = 1f;
+    float stoppingDistance = 0.4f;
+    bool enableAnim = false;
+    bool stop = false;
 
     [SerializeField] GameObject door;
-    [SerializeField] GameObject open;
     Animator doorAnimator;
     AudioSource doorAudio;
 
+    [SerializeField] GameObject open;
+    [SerializeField] GameObject alarm;
 
+    [SerializeField] GameObject light;
+    Animator lightAnimator;
+    AudioSource lightAudio;
+    bool activeAlarm = false;
+    
     // 
     void Start()
     {
         nicoleAnimator = nicole.GetComponent<Animator>();
         cooperAnimator = cooper.GetComponent<Animator>();
         doorAnimator = door.GetComponent<Animator>();
+        lightAnimator = light.GetComponent<Animator>();
         nicoleAudio = nicole.GetComponent<AudioSource>();
         cooperAudio = cooper.GetComponent<AudioSource>();
         doorAudio = door.GetComponent<AudioSource>();
+        lightAudio = light.GetComponent<AudioSource>();
 
         setCamPriority(1, 0);
         StartCoroutine(waitToStart());
@@ -38,11 +58,57 @@ public class record8Controller : MonoBehaviour
     // 
     void Update()
     {
-        if (open.activeInHierarchy)
+        if (open.activeInHierarchy && !enableAnim)
         {
-            //cooper anda hasta la pos destino
-            //audio nicole y animacion de sorpresa
+            cooperAnimator.SetBool("walk", true);
+            rotateNicole = true;
+            StartCoroutine(nicoleActions());
+            enableAnim = true;
         }
+
+        if(enableAnim && !stop)
+        {
+            float distance = Vector3.Distance(cooper.transform.position, targetPosition);
+            if (distance > stoppingDistance)
+            {
+                Vector3 direction = (targetPosition - cooper.transform.position).normalized;
+                cooper.transform.position += direction * moveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                cooperAnimator.SetBool("walk", false);
+
+                // cooper libera un gas y duerme a nicole
+
+                stop = true;
+            }
+        }
+
+        if (rotateNicole && nicole.transform.rotation != nicoleRotation)
+        {
+            nicole.transform.rotation = Quaternion.RotateTowards(nicole.transform.rotation, nicoleRotation, rotationSpeed * Time.deltaTime * 100f);
+        } else if (nicole.transform.rotation == nicoleRotation)
+        {
+            rotateNicole = false;
+        }
+
+        if (rotateNicole2 && nicole.transform.rotation != nicoleRotation2)
+        {
+            nicole.transform.rotation = Quaternion.RotateTowards(nicole.transform.rotation, nicoleRotation2, rotationSpeed * Time.deltaTime * 100f);
+        } else if (nicole.transform.rotation == nicoleRotation2 && !pressBtn)
+        {
+            nicoleAnimator.SetBool("push", true);
+            rotateNicole2 = false;
+            pressBtn = true;
+        }
+
+        if (alarm.activeInHierarchy && !activeAlarm)
+        {
+            lightAnimator.SetBool("alarm", true);
+            lightAudio.Play();
+            activeAlarm = true;
+        }
+
     }
 
     IEnumerator waitToStart()
@@ -55,6 +121,20 @@ public class record8Controller : MonoBehaviour
         yield return new WaitForSeconds(3f);
         doorAnimator.SetBool("up", true);
         doorAudio.Play();
+    }
+
+    //
+    IEnumerator nicoleActions()
+    {
+        yield return new WaitForSeconds(2f);
+        nicoleAudio.Play();
+        nicoleAnimator.SetBool("focus", true);
+        yield return new WaitForSeconds(3f);
+        rotateNicole2 = true;
+        yield return new WaitForSeconds(0.2f);
+        nicoleAnimator.SetBool("push", true);
+
+        //nicole activa una alarma
     }
 
     // Sets camera priority.
