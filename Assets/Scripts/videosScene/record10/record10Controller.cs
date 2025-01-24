20d6e4ebeb687a4ef84b1983631be353a38e604a
Sandra Ciudad <sandra.ciudad1@alu.uclm.es>
2025-01-24 10:56:01 +0100

    add record10 animations

--- Assets/Scripts/videosScene/record10/record10Controller.cs
+++ Assets/Scripts/videosScene/record10/record10Controller.cs
@@ -1,28 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class record10Controller : MonoBehaviour
 {
     Animator animator;
 
-    // Start is called before the first frame update
+    [SerializeField] CinemachineVirtualCamera vcam13;
+    [SerializeField] CinemachineVirtualCamera vcam14;
+
+    // Initializes values and starts the coroutine.
     void Start()
     {
         animator = GetComponent<Animator>();
+        setCamPriority(1, 0);
         StartCoroutine(waitToStart());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Waits 5 seconds to start Austin's animations
     IEnumerator waitToStart()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
+        setCamPriority(0, 1);
         animator.SetBool("getUp", true);
+    }
 
+    // Sets camera priority.
+    void setCamPriority(int cam1, int cam2)
+    {
+        vcam13.Priority = cam1;
+        vcam14.Priority = cam2;
     }
 }
