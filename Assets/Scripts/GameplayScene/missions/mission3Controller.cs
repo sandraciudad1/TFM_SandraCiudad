2f8937814875e6a5375fba79f0e39608bc7d571d
Sandra Ciudad <sandra.ciudad1@alu.uclm.es>
2025-03-05 19:49:46 +0100

    uodate mission3 functionalities

--- Assets/Scripts/GameplayScene/missions/mission3Controller.cs
+++ Assets/Scripts/GameplayScene/missions/mission3Controller.cs
@@ -28,7 +28,7 @@ public class mission3Controller : MonoBehaviour
 
     [SerializeField] GameObject gradientBg;
     [SerializeField] RectTransform gradient;
-    [SerializeField] GameObject arrow;
+    [SerializeField] RectTransform arrow;
     float minX = -375f, maxX = 375f;
     float flechaSpeed = 200f;
     bool movingRight = true;
@@ -55,37 +55,50 @@ public class mission3Controller : MonoBehaviour
         
     }
 
+    void OnRectTransformDimensionsChange()
+    {
+        calculateLimits();
+    }
+
     void calculateLimits()
     {
         float halfWidth = gradient.rect.width / 2f;
-        minX = gradient.position.x - halfWidth + 20f;
-        maxX = gradient.position.x + halfWidth - 20f;
+        minX = -halfWidth + 20f;  
+        maxX = halfWidth - 20f;
     }
 
     void Update()
     {
+        Debug.Log("Counter " + counter);
+        Debug.Log("Start " + start + " stopped " + stopped);
+
         if (start && !stopped)
         {
+            Debug.Log("Moviendose");
             moveArrow();
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                Debug.Log("Espacio");
                 counter -= checkValue(arrow.transform.position.x);
 
                 if (counter <= 0)
                 {
+                    Debug.Log("Contador <= 0");
                     solved++;
                     gradientBg.SetActive(false);
                     counter = 3;
                     resetValues();
                     
                 }
+                Debug.Log("stopped se pone a true");
                 stopped = true;
             }
         }
 
-        if (solved == 1 && !updatePos)
+        if (solved == 1 && !updatePos && !stopped)
         {
+            Debug.Log("Solved==1");
             player.transform.position = secondPos;
             SwapCameras(0, 0, 1, 0);
             start = true;
@@ -93,6 +106,7 @@ public class mission3Controller : MonoBehaviour
         } 
         else if (solved == 2 && !updatePos)
         {
+            Debug.Log("Solved==2");
             player.transform.position = thirdPos;
             SwapCameras(0, 0, 0, 1);
             start = true;
@@ -100,32 +114,39 @@ public class mission3Controller : MonoBehaviour
         } 
         else if (solved == 3 && !updatePos)
         {
+            Debug.Log("Solved==3");
             SwapCameras(1, 0, 0, 0);
             playerMov.canMove = true;
             cc.enabled = true;
             updatePos = true;
         }
     }
-
+    
     float checkValue(float value)
     {
         if (value > 296.25f && value < 493.75f) // verde
         {
+            Debug.Log("Verde");
             // hacer animacion de apretar y se corta el humo
             updateSmoke(0.1f);
             return 1;  
         }
         else if ((value > 128.38f && value <= 296.25f) || (value >= 493.75f && value < 661.63f)) // amarillo 
         {
+            Debug.Log("Amarillo");
             // quitar vida y decrementar en 0.5
             updateSmoke(0.05f);
             return 0.5f;
         }
-        else // rojo
+        else if(value <= 128.38f || value >= 661.63f) // rojo
         {
+            Debug.Log("Rojo");
             StartCoroutine(waitToReset());
             return 0;
-
+        } 
+        else
+        {
+            return 0;
         }
     }
 
@@ -136,28 +157,33 @@ public class mission3Controller : MonoBehaviour
         if (solved == 0)
         {
             smoke1.transform.localScale -= new Vector3(value, value, value);
+            Debug.Log("Reduciendo smoke 1 " + smoke1.transform.localScale);
         } 
         else if (solved == 1)
         {
             smoke2.transform.localScale -= new Vector3(value, value, value);
             smoke3.transform.localScale -= new Vector3(value, value, value);
+            Debug.Log("Reduciendo smoke 2 y 3 " + smoke2.transform.localScale + " " + smoke3.transform.localScale);
         } else if (solved == 2)
         {
             smoke4.transform.localScale -= new Vector3(value, value, value);
+            Debug.Log("Reduciendo smoke 4 " + smoke4.transform.localScale);
         }
         
     }
 
     IEnumerator waitUntilFinishAnim()
     {
+        Debug.Log("Esperando 9 segundos a la animacion");
         yield return new WaitForSeconds(9.1f);
         StartCoroutine(waitToReset());
     }
 
     IEnumerator waitToReset()
     {
-        resetValues();
+        Debug.Log("Esperando a resetear");
         yield return new WaitForSeconds(0.5f);
+        resetValues();
         start = true;
     }
 
@@ -167,24 +193,26 @@ public class mission3Controller : MonoBehaviour
 
         if (movingRight)
         {
-            arrow.transform.position += new Vector3(step, 0, 0);
-            if (arrow.transform.position.x >= maxX)
+            arrow.anchoredPosition += new Vector2(step, 0);
+            if (arrow.anchoredPosition.x >= maxX)
                 movingRight = false;
         }
         else
         {
-            arrow.transform.position -= new Vector3(step, 0, 0);
-            if (arrow.transform.position.x <= minX)
+            arrow.anchoredPosition -= new Vector2(step, 0);
+            if (arrow.anchoredPosition.x <= minX)
                 movingRight = true;
         }
     }
 
     void resetValues()
     {
+        Debug.Log("Reset");
+        playerAnim.SetBool("wrench", false);
         start = false;
+        Debug.Log("stopped se pone a FALSE");
         stopped = false;
         arrow.transform.position = initialArrowPos;
-        
         updatePos = false;
     }
 
@@ -220,15 +248,8 @@ public class mission3Controller : MonoBehaviour
             {
                 gradientBg.SetActive(true);
                 start = true;
-                //playerAnim.SetBool("wrench", true);
                 change = true;
             }
-            
-            //
-            // se muestra 3 veces la barra
-            //desactivar el character controller par acolocarlo bien
-            // por cada vez que el usuario ajuste bien la presio se muestra la animacion de apretar
-            // a la tercera vez de apretar bien se corta el humo y se va a la sigueinte barra
         }
     }
 
