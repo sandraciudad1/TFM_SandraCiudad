a23a0130d6b2fe0bc423b52faf322a14a34f272e
Sandra Ciudad <sandra.ciudad1@alu.uclm.es>
2025-03-06 09:49:49 +0100

    finish mision3 logic

--- Assets/Scripts/GameplayScene/missions/mission3Controller.cs
+++ Assets/Scripts/GameplayScene/missions/mission3Controller.cs
@@ -10,9 +10,9 @@ public class mission3Controller : MonoBehaviour
     Animator playerAnim;
     CharacterController cc;
     PlayerMovement playerMov;
-    Vector3 firstPos = new Vector3(88.028f, 30.74504f, 62.476f);
-    Vector3 secondPos = new Vector3(88.028f, 30.74504f, 60.484f);
-    Vector3 thirdPos = new Vector3(88.028f, 30.74504f, 59.8f);
+    Vector3 firstPos = new Vector3(88.10962f, 30.74504f, 62.43017f);
+    Vector3 secondPos = new Vector3(87.97955f, 30.74504f, 60.40985f);
+    Vector3 thirdPos = new Vector3(87.49987f, 30.74504f, 57.9805f);
     Quaternion playerRot = Quaternion.Euler(new Vector3(0f, 90f, 0f));
     bool change = false;
     bool updatePos = false;
@@ -69,36 +69,18 @@ public class mission3Controller : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("Counter " + counter);
-        Debug.Log("Start " + start + " stopped " + stopped);
-
         if (start && !stopped)
         {
-            Debug.Log("Moviendose");
             moveArrow();
-
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log("Espacio");
-                counter -= checkValue(arrow.transform.position.x);
-
-                if (counter <= 0)
-                {
-                    Debug.Log("Contador <= 0");
-                    solved++;
-                    gradientBg.SetActive(false);
-                    counter = 3;
-                    resetValues();
-                    
-                }
-                Debug.Log("stopped se pone a true");
+                checkValue(arrow.transform.position.x);
                 stopped = true;
             }
         }
 
         if (solved == 1 && !updatePos && !stopped)
         {
-            Debug.Log("Solved==1");
             player.transform.position = secondPos;
             SwapCameras(0, 0, 1, 0);
             start = true;
@@ -106,7 +88,6 @@ public class mission3Controller : MonoBehaviour
         } 
         else if (solved == 2 && !updatePos)
         {
-            Debug.Log("Solved==2");
             player.transform.position = thirdPos;
             SwapCameras(0, 0, 0, 1);
             start = true;
@@ -114,40 +95,48 @@ public class mission3Controller : MonoBehaviour
         } 
         else if (solved == 3 && !updatePos)
         {
-            Debug.Log("Solved==3");
+            gradientBg.SetActive(false);
             SwapCameras(1, 0, 0, 0);
             playerMov.canMove = true;
             cc.enabled = true;
             updatePos = true;
         }
     }
+
     
-    float checkValue(float value)
+    
+    void checkValue(float value)
     {
         if (value > 296.25f && value < 493.75f) // verde
         {
-            Debug.Log("Verde");
-            // hacer animacion de apretar y se corta el humo
             updateSmoke(0.1f);
-            return 1;  
+            counter -= 1;  
         }
         else if ((value > 128.38f && value <= 296.25f) || (value >= 493.75f && value < 661.63f)) // amarillo 
         {
-            Debug.Log("Amarillo");
-            // quitar vida y decrementar en 0.5
             updateSmoke(0.05f);
-            return 0.5f;
+            counter -= 0.5f;
         }
         else if(value <= 128.38f || value >= 661.63f) // rojo
         {
-            Debug.Log("Rojo");
             StartCoroutine(waitToReset());
-            return 0;
-        } 
-        else
-        {
-            return 0;
         }
+
+        if (counter <= 0)
+        {
+            gradientBg.SetActive(false);
+            StartCoroutine(waitUntilChange());
+        }
+    }
+
+    IEnumerator waitUntilChange()
+    {
+        yield return new WaitForSeconds(10f);
+        counter = 3;
+        resetValues();
+        solved++;
+        gradientBg.SetActive(true);
+        start = true;
     }
 
     void updateSmoke(float value)
@@ -157,31 +146,26 @@ public class mission3Controller : MonoBehaviour
         if (solved == 0)
         {
             smoke1.transform.localScale -= new Vector3(value, value, value);
-            Debug.Log("Reduciendo smoke 1 " + smoke1.transform.localScale);
         } 
         else if (solved == 1)
         {
             smoke2.transform.localScale -= new Vector3(value, value, value);
             smoke3.transform.localScale -= new Vector3(value, value, value);
-            Debug.Log("Reduciendo smoke 2 y 3 " + smoke2.transform.localScale + " " + smoke3.transform.localScale);
-        } else if (solved == 2)
+        } 
+        else if (solved == 2)
         {
             smoke4.transform.localScale -= new Vector3(value, value, value);
-            Debug.Log("Reduciendo smoke 4 " + smoke4.transform.localScale);
         }
-        
     }
 
     IEnumerator waitUntilFinishAnim()
     {
-        Debug.Log("Esperando 9 segundos a la animacion");
         yield return new WaitForSeconds(9.1f);
         StartCoroutine(waitToReset());
     }
 
     IEnumerator waitToReset()
     {
-        Debug.Log("Esperando a resetear");
         yield return new WaitForSeconds(0.5f);
         resetValues();
         start = true;
@@ -207,10 +191,8 @@ public class mission3Controller : MonoBehaviour
 
     void resetValues()
     {
-        Debug.Log("Reset");
         playerAnim.SetBool("wrench", false);
         start = false;
-        Debug.Log("stopped se pone a FALSE");
         stopped = false;
         arrow.transform.position = initialArrowPos;
         updatePos = false;
@@ -239,6 +221,7 @@ public class mission3Controller : MonoBehaviour
     {
         if (other.gameObject.CompareTag("modularPipes") && spannerwrench.activeInHierarchy && Input.GetKeyDown(KeyCode.X))
         {
+            letterX.SetActive(false);
             SwapCameras(0, 1, 0, 0);
             playerMov.canMove = false;
             cc.enabled = false;
